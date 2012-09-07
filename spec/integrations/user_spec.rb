e50634f6ccaeac350e10e10c508cbef29da16109
Shogo Ohta <os0x@ss-o.net>
2012-09-07 18:44:15 +0900

    add sample spec

--- spec/integrations/user_spec.rb
+++ spec/integrations/user_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 
 describe 'User' do
   let(:user) {'tester'}
+
   context 'login' do
     it 'can login' do
       visit top_path
       fill_in 'name', :with => user
       click_button 'login'
       page.should have_content("Your name is #{user}")
+    end
+  end
+
+  context 'post' do
+    it 'can post article' do
+      visit top_path
+      fill_in 'name', :with => user
+      click_button 'login'
+      click_button 'New article'
+      fill_in 'article_title', :with => 'sample-title'
+      fill_in 'article_content', :with => 'sample-body'
+      click_button 'Create article'
+      page.should have_css('h2.article_title', :text => 'sample-title')
+      page.should have_css('.attributes', :text => user)
     end
   end
 end
