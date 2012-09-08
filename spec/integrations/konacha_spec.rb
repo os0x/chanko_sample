163eab3d0f6419f820d8b97ccc56e3b06de77a54
Shogo Ohta <os0x@ss-o.net>
2012-09-08 16:15:24 +0900

    fix indent

--- spec/integrations/konacha_spec.rb
+++ spec/integrations/konacha_spec.rb
@@ -8,13 +8,17 @@ describe 'javascripts' do
       if /\.coffee$/ === file
         spec = CoffeeScript.compile(spec)
       end
-      spec << ';(function() {var r = mocha.run();r.globals(["CapybaraInvocation"]); return r.total;}());'
-      # puts spec
+      spec << <<-EOS
+;(function() {
+  var r = mocha.run();
+  r.globals(["CapybaraInvocation"]);
+  return r.total;
+}());
+      EOS
       spec =~ %r|^\n*/\*\n([^*]+)\*/.*|
       command = $1
       eval(command)
       page.status_code.should == 200
-      page.execute_script "$('<div id=\"mocha\" />').appendTo('body')"
       total = page.evaluate_script spec
       page.should have_css('#report li.test.pass', :count => total)
       page.should_not have_css('#report li.test.fail')
